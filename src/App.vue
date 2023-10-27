d3ec24bec5fe61e4adeb695517073cd96abbe3ec
47483 <47483@gymnasium.ax>
2023-10-27 13:27:06 +0300

    Began Weather

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>H3mlins väderapp</h1>
+    <p class="center">När Kjell ger väder lyssnar man</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <p>NOERRPLS</p>
+</template>
+
+<style scoped>
+
+</style>
