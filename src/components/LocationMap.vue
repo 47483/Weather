481489f7daf8f61994f93e2b4ddcabfa5c0c605b
47483 <47483@gymnasium.ax>
2023-11-03 09:00:17 +0200

    Fixed Map Resizing And Marker Placement

--- src/components/LocationMap.vue
+++ src/components/LocationMap.vue
@@ -1,27 +1,51 @@
 <script setup>
-const props = defineProps(['lat', 'long'])
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+let locationMap;
+let marker;
+
+window.onload = function() {
+    locationMap = L.map('map').setView([15,15], 12);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(locationMap);
+    UpdateMap();
+    locationMap.on('click', MapMarker);
+
+    document.body.onresize = function() {
+      UpdateMap();
+    }
+}
+
+function UpdateMap() {
+  let mapRef = document.getElementById("map");
+  let mapWrapRef = document.getElementById("mapWrapper");
+  mapRef.style.width = mapWrapRef.clientWidth+"px";
+  mapRef.style.height = mapWrapRef.clientHeight+"px";
+
+  locationMap.invalidateSize();
+}
+
+function MapMarker(e) {
+  if (marker) {
+    locationMap.removeLayer(marker);
+  }
+
+  marker = L.marker(e.latlng).addTo(locationMap);
+}
 </script>
 
 <template>
-  <div id="mapWrapper">
-    <iframe
-      id="map"
-      src="https://www.google.com/maps/embed/v1/place?q=5,5"
-      allowfullscreen=""
-    ></iframe>
-  </div>
+    <div id="mapWrapper"><div id="map"></div></div>
 </template>
 
 <style scoped>
 #mapWrapper {
-  position: relative;
-  height: 16em;
-  padding: 0.5em;
+  margin: 0.5em;
+  height: 15em;
+  overflow: hidden;
 }
 
 #map {
-  width: 100%;
-  height: 100%;
   border-radius: 0.5em;
 }
 </style>
